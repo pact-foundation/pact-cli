aba180302d0eb586a93ef5636e72cefeb12d1533
Yousaf Nabi <you@saf.dev>
2026-01-08 20:06:26 +0000

    chore(ps1): add content type header
    Added 'Content-Type' header to the web request for Pact CLI.

--- install.ps1
+++ install.ps1
@@ -4,7 +4,7 @@ $pactDir = $pwd.Path
 
 Write-Host "--> Downloading Latest Pact broker Client binary)"
 
-$latestRelease = Invoke-WebRequest https://github.com/pact-foundation/pact-cli/releases/latest -Headers @{"Accept"="application/json"}
+$latestRelease = Invoke-WebRequest https://github.com/pact-foundation/pact-cli/releases/latest -Headers @{"Accept"="application/json" 'Content-Type' = 'application/json'}
 $json = $latestRelease.Content | ConvertFrom-Json
 $tag = $json.tag_name
 $architecture = [System.Runtime.InteropServices.RuntimeInformation,mscorlib]::OSArchitecture.ToString().ToLower()
